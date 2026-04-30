fd73db5ee43336942397faaf3b26f3cf555e02ad
nedzte07-eng <nedzte07@gmail.com>
2026-04-30 15:29:59 +0300

    order create not finished

--- tests/ui_tests/pages/order_create.py
+++ tests/ui_tests/pages/order_create.py
@@ -4,7 +4,20 @@ class OrderCreatePage:
     def __init__(self, page:Page):
         self._page = page
         self._title = page.locator('h4.page-title')
+        self._number_of_adults = page.locator('input#adultsCount')
+        self._number_of_children = page.locator('input#childrenCount')
+        self._input_start_date = page.locator('input#startDate')
+        self._input_end_date = page.locator('input#endDate')
+        self._create_button = page.locator('//button[@class="btn btn-primary"]')
 
     def get_title(self):
         return self._title
+
+    def create_gvv_with_2_adults(self, start_date:str, end_date:str):
+        self._number_of_adults.fill('2')
+        self._input_start_date.fill(start_date)
+        self._input_end_date.fill(end_date)
+        self._create_button.click()
+
+
 
